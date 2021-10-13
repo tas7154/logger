a885f233868212b219e2cfd82be91d9bbe6ab176
tas7154 <theaquarianspace@gmail.com>
2021-10-13 17:33:56 +0000

    added logger file

---
+++ app.js
@@ -0,0 +1,10 @@
+const logger = require('./logger');
+
+var message = 'message';
+//console.log(message); 
+logger(message); // export passing a single function
+//console.log(module); //global
+
+
+
+
---
+++ logger.js
@@ -0,0 +1,5 @@
+function log(message) {
+    console.log(message);
+}
+//module.exports.log = log; - exports an object
+module.exports = log; //exports single function
